a6dca1ae434cc2ef9ef5e2be06905e3bc3a99dbb
Teun van den Brand <tahvdbrand@gmail.com>
2025-09-14 14:06:43 +0200

    adapt to dynamic theming

--- R/zzz.R
+++ R/zzz.R
@@ -13,6 +13,25 @@ on_load({
   }
 })
 
+on_load({
+  if (exists("element_geom", asNamespace("ggplot2"))) {
+    new_defaults <- aes(
+      colour        = from_theme(fill %||% ink),
+      linewidth     = from_theme(linewidth * 2),
+      linetype      = from_theme(linetype),
+      stroke_colour = from_theme(colour %||% NA),
+      stroke_width  = from_theme(borderwidth / 2)
+    )
+    update_geom_defaults(GeomArrow, new_defaults)
+    update_geom_defaults(GeomArrowChain, aes(
+      !!!new_defaults,
+      size          = from_theme(pointsize)
+    ))
+    update_geom_defaults(GeomArrowCurve, new_defaults)
+    update_geom_defaults(GeomArrowSegment, new_defaults)
+  }
+})
+
 .onLoad <- function(lib, pkg) {
   run_on_load()
 }
